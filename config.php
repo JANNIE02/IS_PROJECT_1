79f6be8d3e3199af1f488745eed3a83f6a6d848b
Sherly Mosoti <sherly.mosoti@strathmore.edu>
2026-06-29 18:57:28 +0300

    added pages

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+
+$conf['site_name'] = "Wes";
+$conf['site_url'] = "www.westonhotel.com";
+$conf['admin_email'] = "sherly.mosoti@strathmore.edu";
+
+//site language
+$conf['language'] = "en";
+//database constants
+$conf['db_type'] = "pgsql";
+$conf['db_host'] = "localhost";
+$conf['db_port'] = "5432"; 
+$conf['db_user'] = "postgres";
+$conf['db_pass'] = "Mokoro111*";
+$conf['db_name'] = "api_groupwork";
